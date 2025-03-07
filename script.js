882c0875cd839f410360ca91aa37e424bfb1462a
Fanuel Seyoum <197907161+fanuel-seyoum-2@users.noreply.github.com>
2025-03-07 17:29:46 +0300

    Add functionality to compare human and computer choices

--- script.js
+++ script.js
@@ -23,6 +23,39 @@ function getComputerChoice() {
  */
 function getHumanChoice() {
 	let choice = prompt(`Pick one: 
-	Rock | Paper } Scissors`, "Scissors");
+	Rock | Paper | Scissors`, "Scissors");
 	return choice.toLowerCase();
+}
+/*
+ * playRound
+ * Compares the parameters and displays the winning party
+ *
+ * Spelling errors on the part of the user
+ * are counted as a "loss"
+ */
+function playRound(humanChoice, computerChoice) {
+	let roundResult = "loss";
+
+	if (humanChoice == computerChoice) {
+		roundResult = "tie";
+		console.log("It's a tie");
+	}
+
+	else if (
+			(humanChoice == "rock" && computerChoice == "scissors") || 
+			(humanChoice == "paper" && computerChoice == "rock")  ||
+			(humanChoice == "scissors" && computerChoice == "paper")
+		) {
+		
+		roundResult = "win";
+		console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+	}
+	else {
+		console.log(`You lose! ${computerChoice} beats ${humanChoice} 
+		Either that, or ${humanChoice} isn't playable in this game!
+		Check that you spelled it correctly!
+		`);
+	}
+
+	return roundResult;
 }
